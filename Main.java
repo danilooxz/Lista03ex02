dbe55c920b2886d0dd00736af53870b0f0200fb0
Danilo Marcon Júnior <128654904+danilooxz@users.noreply.github.com>
2024-04-04 22:32:12 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,14 @@ package br.cesul;
 
 public class Main {
     public static void main(String[] args) {
-        Election election = new Election(50, 40, 20, 10);
-        System.out.println("O canditado campeão foi o A com : " + election.candidateA);
-        System.out.println("O cantidado B ficou com : " + election.candidateB);
-        System.out.println("A quantidade de votos nulos ficou em: " + election.voteNull);
-        System.out.println("A quantidade de voltos em brancos ficow em: " + election.voteWhite);
-        System.out.println((election.checkResult()));
+        Election eleicao = new Election();
+
+        eleicao.votarCandidatoA();
+        eleicao.votarCandidatoA();
+        eleicao.votarCandidatoB();
+        eleicao.votarBranco();
+        eleicao.votarNulo();
+
+        System.out.println("Resultado da eleição: " + eleicao.apurarResultado());
     }
 }
